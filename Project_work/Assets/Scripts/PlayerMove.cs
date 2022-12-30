08c9b2d4ac96b54db1f9ef56be5adabc14789e8e
Theresa <theresa.dietinger@gmail.com>
2022-12-30 22:45:04 +0100

    [#6] add jump functionality

--- Project_work/Assets/Scripts/PlayerMove.cs
+++ Project_work/Assets/Scripts/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,12 @@ public class PlayerMove : MonoBehaviour
 {
 
     [SerializeField] private float walkSpeed = 10f;
+    [SerializeField] private float jumpPower = 5;
 
     private Vector2 moveInput;
-
     private Rigidbody rb;
+    private bool isGrounded = true;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +24,31 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         Run();
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump(); 
+        }
+        
     }
 
     void Run()
     {
         Vector3 playerVelocity = new Vector3(moveInput.x * walkSpeed, rb.velocity.y, moveInput.y * walkSpeed);
         rb.velocity = transform.TransformDirection(playerVelocity);
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        isGrounded = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 
     void OnMove( InputValue value)
