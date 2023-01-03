b513201ab7f0cb564d57b590cb771b1edec9062a
Theresa <theresa.dietinger@gmail.com>
2023-01-03 14:16:20 +0100

    [#8] implement basic overlay for spawning objects

--- Project_work/Assets/Scripts/SpawnerScript.cs
+++ Project_work/Assets/Scripts/SpawnerScript.cs
@@ -7,21 +7,29 @@ public class SpawnerScript : MonoBehaviour
     
     public GameObject cubePrefab;
     public GameObject spherePrefab;
+    public GameObject overlay;
     public static string objectID;
+    private bool overlayActive = false;
     
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            overlayActive = !overlayActive;
+            overlay.SetActive(overlayActive);
+        }
+
+        if (overlayActive && Input.GetKeyDown(KeyCode.Mouse0))
+        {
             // If you want to run the game without the rfid reader uncomment one of the line below
             
             // Uncomment the line below for getting the id for the cube
-            // objectID = "670093BEAFE5";
+            objectID = "670093BEAFE5";
             
             // Uncomment he line below for getting the id for the sphere
             // objectID = "67009C404AF1";
-            
-            
+
+
             switch (objectID)
             {
                 case "670093BEAFE5": 
