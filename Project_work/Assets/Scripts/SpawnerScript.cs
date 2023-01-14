3a7cbce5fe04ee2deb5eb87288b93441fa57e645
Theresa <theresa.dietinger@gmail.com>
2023-01-14 17:01:09 +0100

    [#12] create logic for showing outlines of objects

--- Project_work/Assets/Scripts/SpawnerScript.cs
+++ Project_work/Assets/Scripts/SpawnerScript.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Objects
+{
+    CUBE,
+    SPHERE,
+    NONE
+}
 public class SpawnerScript : MonoBehaviour
 {
     
     public GameObject cubePrefab;
     public GameObject spherePrefab;
     public GameObject overlay;
+    public GameObject cubeOutline;
+    public GameObject sphereOutline;
+    
     public static string objectID;
+    
+    
     private bool overlayActive = false;
     
     private Vector3 mousePos;
@@ -16,33 +27,44 @@ public class SpawnerScript : MonoBehaviour
     
     void Update()
     {
+        Objects currentObject = CheckObjectId(objectID);
+        
         if (Input.GetKeyDown(KeyCode.E))
         {
             overlayActive = !overlayActive;
             overlay.SetActive(overlayActive);
+            sphereOutline.SetActive(false);
+            cubeOutline.SetActive(false);
+            
+            switch (currentObject)
+            {
+                case Objects.CUBE:
+                    cubeOutline.SetActive(overlayActive);
+                    break;
+                case Objects.SPHERE:
+                    sphereOutline.SetActive(overlayActive);
+                    break;
+                default:
+                    sphereOutline.SetActive(false);
+                    cubeOutline.SetActive(false);
+                    break;
+            }
         }
 
         if (overlayActive && Input.GetKeyDown(KeyCode.Mouse0))
         {
-            // If you want to run the game without the rfid reader uncomment one of the line below
             
-            // Uncomment the line below for getting the id for the cube
-            objectID = "670093BEAFE5";
-            
-            // Uncomment he line below for getting the id for the sphere
-            // objectID = "67009C404AF1";
-
             mousePos = Input.mousePosition;
             mousePos.z = 2.0f;
             objectPos = Camera.main.ScreenToWorldPoint(mousePos);
             
-            switch (objectID)
+            switch (currentObject)
             {
-                case "670093BEAFE5": 
+                case Objects.CUBE: 
                     Debug.Log("Spawn cube");
                     Instantiate(cubePrefab, objectPos, Quaternion.identity);
                     break;
-                case "67009C404AF1":
+                case Objects.SPHERE:
                     Debug.Log("Spawn sphere");
                     Instantiate(spherePrefab, objectPos, Quaternion.identity);
                     break;
@@ -50,6 +72,29 @@ public class SpawnerScript : MonoBehaviour
                     Debug.LogError("Something went wrong");
                     break;
             }
+        }
+    }
+
+    private Objects CheckObjectId(string objectID)
+    {
+        // If you want to run the game without the rfid reader uncomment one of the line below
+            
+        // Uncomment the line below for getting the id for the cube
+        objectID = "670093BEAFE5";
+            
+        // Uncomment he line below for getting the id for the sphere
+        // objectID = "67009C404AF1";
+        
+        
+        
+        switch (objectID)
+        {
+            case "670093BEAFE5":
+                return Objects.CUBE;
+            case "67009C404AF1":
+                return Objects.SPHERE;
+            default:
+                return Objects.NONE;
         }
     }
 
