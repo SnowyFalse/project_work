e48766615e4507ec8cbd02361f6395747b32a2b9
Theresa <theresa.dietinger@gmail.com>
2023-01-17 12:14:21 +0100

    [#12] fix spawning logic

--- Project_work/Assets/Scripts/SpawnerScript.cs
+++ Project_work/Assets/Scripts/SpawnerScript.cs
@@ -33,22 +33,13 @@ public class SpawnerScript : MonoBehaviour
         {
             overlayActive = !overlayActive;
             overlay.SetActive(overlayActive);
-            sphereOutline.SetActive(false);
-            cubeOutline.SetActive(false);
             
-            switch (currentObject)
-            {
-                case Objects.CUBE:
-                    cubeOutline.SetActive(overlayActive);
-                    break;
-                case Objects.SPHERE:
-                    sphereOutline.SetActive(overlayActive);
-                    break;
-                default:
-                    sphereOutline.SetActive(false);
-                    cubeOutline.SetActive(false);
-                    break;
-            }
+        }
+
+        if (!overlayActive)
+        {
+            cubeOutline.SetActive(false);
+            sphereOutline.SetActive(false);
         }
 
         if (overlayActive && Input.GetKeyDown(KeyCode.Mouse0))
@@ -80,16 +71,26 @@ public class SpawnerScript : MonoBehaviour
         // If you want to run the game without the rfid reader uncomment one of the line below
             
         // Uncomment the line below for getting the id for the cube
-        objectID = "670093BEAFE5";
+        //objectID = "670093BEAFE5";
             
         // Uncomment he line below for getting the id for the sphere
         // objectID = "67009C404AF1";
 
         switch (objectID)
         {
-            case "670093BEAFE5":
+            case "011396FAC8B6":
+                if (overlayActive)
+                {
+                    cubeOutline.SetActive(true);
+                    sphereOutline.SetActive(false);
+                }
                 return Objects.CUBE;
-            case "67009C404AF1":
+            case "011396EA523C":
+                if (overlayActive)
+                {
+                    sphereOutline.SetActive(true);
+                    cubeOutline.SetActive(false);
+                }
                 return Objects.SPHERE;
             default:
                 return Objects.NONE;
