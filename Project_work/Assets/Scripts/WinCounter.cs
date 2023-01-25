a001ee69fe3fa3ff2b1319e80108f1e6c5ce3bf4
Theresa <theresa.dietinger@gmail.com>
2023-01-25 14:39:37 +0100

    adapt level

--- Project_work/Assets/Scripts/WinCounter.cs
+++ Project_work/Assets/Scripts/WinCounter.cs
@@ -6,48 +6,11 @@ using UnityEngine.UI;
 
 public class WinCounter : MonoBehaviour
 {
-    /*
-    public int winCounter;
-    public int overallWinCounter;
-    
-    public Text rockCounter;
-    public GameObject rockImage;
-
-    public static bool canWin = false;
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("WIN COUNTER: " + winCounter);
-        //overallWinCounter = winCounter + WinCounter2.winCounter2;
-        if (winCounter >= 2)
-        {
-            rockCounter.text = "Portal opened!";
-            canWin = true;
-        }
-        else
-        {
-            rockCounter.text = winCounter + "/2 in position WORK";
-        }
-
-        if (SpawnerScript.overlayActive || SpawnerScript.pauseMenuActive)
-        {
-            rockCounter.text = "";
-            rockImage.SetActive(false);
-        }
-        else
-        {
-            rockImage.SetActive(true);
-        }
-    }
-    
-    
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag.Equals("Object"))
         {
 
-            winCounter++;
             FindObjectOfType<AudioManager>().Play("Trap activated");
         }
 
@@ -56,5 +19,4 @@ public class WinCounter : MonoBehaviour
             FindObjectOfType<AudioManager>().Play("Trap activated");
         }
     }
-    */
 }
